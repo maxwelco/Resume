c70166345ceebb4dcaaeb5f697d3a308b6ead515
openweedsci <openweedsci@gmail.edu>
2020-09-25 14:51:04 -0300

    cv

---
+++ Resumee/deleted_codes.R
@@ -0,0 +1,8 @@
+Disclaimer {#disclaimer}
+  --------------------------------------------------------------------------------
+    
+    Made with the R package [**pagedown**](https://github.com/rstudio/pagedown). 
+  
+  The source code is available [on github.com/nstrayer/cv](https://github.com/nstrayer/cv).
+  
+  Last updated on `r Sys.Date()`.
