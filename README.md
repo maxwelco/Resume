00e104dde45e7f7579a61edcc7fabe775db094d0
Maxwel  Coura Oliveira <maxwelco@Maxwels-MacBook-Pro.local>
2021-04-30 09:07:07 -0300

    ip

--- README.md
+++
@@ -1,3 +0,0 @@
-# Resume
-Resume and CVs
- 
--- Resumee/deleted_codes.R
+++
@@ -1,8 +0,0 @@
-Disclaimer {#disclaimer}
-  --------------------------------------------------------------------------------
-    
-    Made with the R package [**pagedown**](https://github.com/rstudio/pagedown). 
-  
-  The source code is available [on github.com/nstrayer/cv](https://github.com/nstrayer/cv).
-  
-  Last updated on `r Sys.Date()`.
